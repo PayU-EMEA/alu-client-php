b6aaa22973439163a72c7c4f40e019d0a46a6319
Catalin Vlad <Catalin.Vlad@payu.ro>
2018-06-28 13:01:12 +0300

    remove duplicate code

--- tests/PayU/Alu/RequestTest.php
+++ tests/PayU/Alu/RequestTest.php
@@ -83,226 +83,17 @@ class RequestTest extends \PHPUnit_Framework_TestCase
 
     public function testGetParams()
     {
-        $result = array(
-            'ALIAS' => NULL,
-            'BACK_REF' => 'http://path/to/your/returnUrlScript',
-            'BILL_ADDRESS' => 'ADDRESS1',
-            'BILL_ADDRESS2' => 'ADDRESS2',
-            'BILL_BANK' => NULL,
-            'BILL_BANKACCOUNT' => NULL,
-            'BILL_CIISSUER' => NULL,
-            'BILL_CINUMBER' => '324322',
-            'BILL_CISERIAL' => NULL,
-            'BILL_CITYPE' => NULL,
-            'BILL_CITY' => 'Bucuresti',
-            'BILL_CNP' => NULL,
-            'BILL_COMPANY' => NULL,
-            'BILL_COUNTRYCODE' => 'RO',
-            'BILL_EMAIL' => 'john.doe@mail.com',
-            'BILL_FAX' => NULL,
-            'BILL_FISCALCODE' => NULL,
-            'BILL_FNAME' => 'John',
-            'BILL_LNAME' => 'Doe',
-            'BILL_PHONE' => '0755167887',
-            'BILL_REGNUMBER' => NULL,
-            'BILL_STATE' => NULL,
-            'BILL_ZIPCODE' => NULL,
-            'CARD_PROGRAM_NAME' => NULL,
-            'CC_CVV' => 123,
-            'CC_NUMBER' => '5431210111111111',
-            'CC_NUMBER_RECIPIENT' => NULL,
-            'CC_OWNER' => 'test',
-            'CLIENT_IP' => '127.0.0.1',
-            'CLIENT_TIME' => '',
-            'DELIVERY_ADDRESS' => 'ADDRESS1',
-            'DELIVERY_ADDRESS2' => 'ADDRESS2',
-            'DELIVERY_CITY' => 'Istanbul',
-            'DELIVERY_COMPANY' => NULL,
-            'DELIVERY_COUNTRYCODE' => 'RO',
-            'DELIVERY_EMAIL' => 'john.doe@mail.com',
-            'DELIVERY_FNAME' => 'John',
-            'DELIVERY_LNAME' => 'Doe',
-            'DELIVERY_PHONE' => '0755167887',
-            'DELIVERY_STATE' => NULL,
-            'DELIVERY_ZIPCODE' => NULL,
-            'DISCOUNT' => NULL,
-            'EXP_MONTH' => '11',
-            'EXP_YEAR' => 2016,
-            'MERCHANT' => 'MERCHANT_CODE',
-            'ORDER_DATE' => '2014-09-19 10:00:00',
-            'ORDER_MPLACE_MERCHANT' =>
-                array (
-                    0 => NULL,
-                    1 => NULL,
-                ),
-            'ORDER_PCODE' =>
-                array (
-                    0 => 'PCODE01',
-                    1 => 'PCODE02',
-                ),
-            'ORDER_PGROUP' =>
-                array (
-                    0 => NULL,
-                    1 => NULL,
-                ),
-            'ORDER_PINFO' =>
-                array (
-                    0 => NULL,
-                    1 => NULL,
-                ),
-            'ORDER_PNAME' =>
-                array (
-                    0 => 'PNAME01',
-                    1 => 'PNAME02',
-                ),
-            'ORDER_PRICE' =>
-                array (
-                    0 => 100,
-                    1 => 200,
-                ),
-            'ORDER_QTY' =>
-                array (
-                    0 => 1,
-                    1 => 1,
-                ),
-            'ORDER_REF' => 'MerchantOrderRef',
-            'ORDER_SHIPPING' => NULL,
-            'ORDER_VER' =>
-                array (
-                    0 => NULL,
-                    1 => NULL,
-                ),
-            'PAY_METHOD' => 'CCVISAMC',
-            'PRICES_CURRENCY' => 'RON',
-            'SELECTED_INSTALLMENTS_NUMBER' => '2',
-            'USE_LOYALTY_POINTS' => NULL,
-            'LOYALTY_POINTS_AMOUNT' => NULL,
-            'CAMPAIGN_TYPE' => 'EXTRA_INSTALLMENTS',
-            'ORDER_PRICE_TYPE' =>
-                array(
-                    0 => 'NET',
-                    1 => 'NET',
-                ),
-            'ORDER_VAT' =>
-                array(
-                    0 => 24,
-                    1 => 24,
-                ),
-        );
+        $result = $this->createExpectedRequest();
 
         $this->assertEquals($result, $this->request->getRequestParams());
     }
 
     public function testGetParamsWithFx()
     {
-        $result = array(
-            'ALIAS' => NULL,
-            'BACK_REF' => 'http://path/to/your/returnUrlScript',
-            'BILL_ADDRESS' => 'ADDRESS1',
-            'BILL_ADDRESS2' => 'ADDRESS2',
-            'BILL_BANK' => NULL,
-            'BILL_BANKACCOUNT' => NULL,
-            'BILL_CIISSUER' => NULL,
-            'BILL_CINUMBER' => '324322',
-            'BILL_CISERIAL' => NULL,
-            'BILL_CITYPE' => NULL,
-            'BILL_CITY' => 'Bucuresti',
-            'BILL_CNP' => NULL,
-            'BILL_COMPANY' => NULL,
-            'BILL_COUNTRYCODE' => 'RO',
-            'BILL_EMAIL' => 'john.doe@mail.com',
-            'BILL_FAX' => NULL,
-            'BILL_FISCALCODE' => NULL,
-            'BILL_FNAME' => 'John',
-            'BILL_LNAME' => 'Doe',
-            'BILL_PHONE' => '0755167887',
-            'BILL_REGNUMBER' => NULL,
-            'BILL_STATE' => NULL,
-            'BILL_ZIPCODE' => NULL,
-            'CARD_PROGRAM_NAME' => NULL,
-            'CC_CVV' => 123,
-            'CC_NUMBER' => '5431210111111111',
-            'CC_NUMBER_RECIPIENT' => NULL,
-            'CC_OWNER' => 'test',
-            'CLIENT_IP' => '127.0.0.1',
-            'CLIENT_TIME' => '',
-            'DELIVERY_ADDRESS' => 'ADDRESS1',
-            'DELIVERY_ADDRESS2' => 'ADDRESS2',
-            'DELIVERY_CITY' => 'Istanbul',
-            'DELIVERY_COMPANY' => NULL,
-            'DELIVERY_COUNTRYCODE' => 'RO',
-            'DELIVERY_EMAIL' => 'john.doe@mail.com',
-            'DELIVERY_FNAME' => 'John',
-            'DELIVERY_LNAME' => 'Doe',
-            'DELIVERY_PHONE' => '0755167887',
-            'DELIVERY_STATE' => NULL,
-            'DELIVERY_ZIPCODE' => NULL,
-            'DISCOUNT' => NULL,
-            'EXP_MONTH' => '11',
-            'EXP_YEAR' => 2016,
-            'MERCHANT' => 'MERCHANT_CODE',
-            'ORDER_DATE' => '2014-09-19 10:00:00',
-            'ORDER_MPLACE_MERCHANT' =>
-                array (
-                    0 => NULL,
-                    1 => NULL,
-                ),
-            'ORDER_PCODE' =>
-                array (
-                    0 => 'PCODE01',
-                    1 => 'PCODE02',
-                ),
-            'ORDER_PGROUP' =>
-                array (
-                    0 => NULL,
-                    1 => NULL,
-                ),
-            'ORDER_PINFO' =>
-                array (
-                    0 => NULL,
-                    1 => NULL,
-                ),
-            'ORDER_PNAME' =>
-                array (
-                    0 => 'PNAME01',
-                    1 => 'PNAME02',
-                ),
-            'ORDER_PRICE' =>
-                array (
-                    0 => 100,
-                    1 => 200,
-                ),
-            'ORDER_QTY' =>
-                array (
-                    0 => 1,
-                    1 => 1,
-                ),
-            'ORDER_REF' => 'MerchantOrderRef',
-            'ORDER_SHIPPING' => NULL,
-            'ORDER_VER' =>
-                array (
-                    0 => NULL,
-                    1 => NULL,
-                ),
-            'PAY_METHOD' => 'CCVISAMC',
-            'PRICES_CURRENCY' => 'RON',
-            'SELECTED_INSTALLMENTS_NUMBER' => '2',
-            'USE_LOYALTY_POINTS' => NULL,
-            'LOYALTY_POINTS_AMOUNT' => NULL,
-            'CAMPAIGN_TYPE' => 'EXTRA_INSTALLMENTS',
-            'ORDER_PRICE_TYPE' =>
-                array(
-                    0 => 'NET',
-                    1 => 'NET',
-                ),
-            'ORDER_VAT' =>
-                array(
-                    0 => 24,
-                    1 => 24,
-                ),
-            'AUTHORIZATION_CURRENCY' => 'EUR',
-            'AUTHORIZATION_EXCHANGE_RATE' => 0.2462,
-        );
+        $result = $this->createExpectedRequest();
+
+        $result['AUTHORIZATION_CURRENCY'] = 'EUR';
+        $result['AUTHORIZATION_EXCHANGE_RATE'] = 0.2462;
 
         $fx = new FX('EUR', 0.2462);
 
@@ -341,7 +132,7 @@ class RequestTest extends \PHPUnit_Framework_TestCase
         );
         $this->order->withAirlineInfo($airlineInfo);
 
-        $result = array(
+        $airlineInfoResult = [
             'AIRLINE_INFO' => array(
                 'PASSENGER_NAME' => 'John Doe',
                 'TICKET_NUMBER' => 'TICKET_1234',
@@ -372,6 +163,21 @@ class RequestTest extends \PHPUnit_Framework_TestCase
                     ),
                 ),
             ),
+        ];
+
+        $expectedRequest = $this->createExpectedRequest();
+
+        $result = array_merge($airlineInfoResult, $expectedRequest);
+
+        $this->assertEquals($result, $this->request->getRequestParams());
+    }
+
+    /**
+     * @return array
+     */
+    public function createExpectedRequest()
+    {
+        $result = array(
             'ALIAS' => NULL,
             'BACK_REF' => 'http://path/to/your/returnUrlScript',
             'BILL_ADDRESS' => 'ADDRESS1',
@@ -419,50 +225,50 @@ class RequestTest extends \PHPUnit_Framework_TestCase
             'MERCHANT' => 'MERCHANT_CODE',
             'ORDER_DATE' => '2014-09-19 10:00:00',
             'ORDER_MPLACE_MERCHANT' =>
-                array (
+                array(
                     0 => NULL,
                     1 => NULL,
                 ),
             'ORDER_PCODE' =>
-                array (
+                array(
                     0 => 'PCODE01',
                     1 => 'PCODE02',
                 ),
             'ORDER_PGROUP' =>
-                array (
+                array(
                     0 => NULL,
                     1 => NULL,
                 ),
             'ORDER_PINFO' =>
-                array (
+                array(
                     0 => NULL,
                     1 => NULL,
                 ),
             'ORDER_PNAME' =>
-                array (
+                array(
                     0 => 'PNAME01',
                     1 => 'PNAME02',
                 ),
             'ORDER_PRICE' =>
-                array (
+                array(
                     0 => 100,
                     1 => 200,
                 ),
             'ORDER_QTY' =>
-                array (
+                array(
                     0 => 1,
                     1 => 1,
                 ),
             'ORDER_REF' => 'MerchantOrderRef',
             'ORDER_SHIPPING' => NULL,
             'ORDER_VER' =>
-                array (
+                array(
                     0 => NULL,
                     1 => NULL,
                 ),
             'PAY_METHOD' => 'CCVISAMC',
             'PRICES_CURRENCY' => 'RON',
-            'SELECTED_INSTALLMENTS_NUMBER' => 2,
+            'SELECTED_INSTALLMENTS_NUMBER' => '2',
             'USE_LOYALTY_POINTS' => NULL,
             'LOYALTY_POINTS_AMOUNT' => NULL,
             'CAMPAIGN_TYPE' => 'EXTRA_INSTALLMENTS',
@@ -477,6 +283,6 @@ class RequestTest extends \PHPUnit_Framework_TestCase
                     1 => 24,
                 ),
         );
-        $this->assertEquals($result, $this->request->getRequestParams());
+        return $result;
     }
 }
