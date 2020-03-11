9aaa949d8758b5ef4cee190009d751349b2278bf
spopawdodo <maria.denisa@gmail.com>
2020-03-11 16:21:20 +0200

    add ResponseParser class which will convert a JsonResponse into an AuthorizationResponse object

---
+++ src/PayU/Payments/Gateways/PaymentsApi/Entities/AuthorizationResponse.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace PayU\Payments\Gateways\PaymentsApi\Entities;
+
+
+class AuthorizationResponse
+{
+    /**
+     * @var array
+     */
+    private $response;
+
+    /**
+     * AuthorizationResponse constructor.
+     * @param array $response
+     */
+    public function __construct(array $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @return array
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+}
