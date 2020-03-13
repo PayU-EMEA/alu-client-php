8c987e4653f5862928c7b541b895255111c586c6
spopawdodo <maria.denisa@gmail.com>
2020-03-13 18:19:05 +0200

    move Authorization process from PayU/Alu/Client to AluV3/AluV3Gateway

--- src/PayU/Payments/Gateways/AluV3/AluV3Gateway.php
+++ src/PayU/Payments/Gateways/AluV3/AluV3Gateway.php
@@ -4,10 +4,120 @@
 namespace AluV3;
 
 
-class AluV3Gateway
-{
-    public function __construct()
-    {
+use AluV3\Services\HashService;
+use AluV3\Services\HTTPClient;
+use AluV3\Services\RequestBuilder;
+use AluV3\Services\ResponseBuilder;
+use AluV3\Services\ResponseParser;
+use PayU\Alu\Exceptions\ClientException;
+use PayU\Alu\Exceptions\ConnectionException;
+use PayU\Alu\Request;
+use PayU\Alu\Response;
+use PayU\Payments\Interfaces\GatewayInterface;
+use SimpleXMLElement;
 
+class AluV3Gateway implements GatewayInterface
+{
+    const ALU_URL_PATH = '/order/alu/v3';
+
+    /**
+     * @var array
+     * todo set ro to original value
+     */
+    private $aluUrlHostname = array(
+        //'ro' => 'https://secure.payu.ro',
+        'ro' => 'http://ro.payu.local',
+        'ru' => 'https://secure.payu.ru',
+        'ua' => 'https://secure.payu.ua',
+        'hu' => 'https://secure.payu.hu',
+        'tr' => 'https://secure.payu.com.tr',
+    );
+
+    /**
+     * @var HTTPClient
+     */
+    private $httpClient;
+
+    /**
+     * @var HashService
+     */
+    private $hashService;
+
+    /**
+     * @var RequestBuilder
+     */
+    private $requestBuilder;
+
+    /**
+     * @var ResponseBuilder
+     */
+    private $responseBuilder;
+
+    /**
+     * @var ResponseParser
+     */
+    private $responseParser;
+
+    public function __construct(
+        HTTPClient $httpClient,
+        HashService $hashService
+    )
+    {
+        $this->httpClient = $httpClient;
+        $this->hashService = $hashService;
+        $this->requestBuilder = new RequestBuilder();
+        $this->responseParser = new ResponseParser();
+        $this->responseBuilder = new ResponseBuilder();
+    }
+
+    /**
+     * @inheritDoc
+     * @throws \PayU\Alu\Exceptions\ConnectionException
+     */
+    public function authorize(Request $request)
+    {
+        $requestArray = $this->requestBuilder->buildAuthorizationRequest($request);
+
+        $requestHash = $this->hashService->makeRequestHash($requestArray);
+
+        $this->requestBuilder->setOrderHash($requestHash);
+
+        $requestParams = $this->requestBuilder->buildAuthorizationRequest($request);
+        try {
+            $responseXML = $this->httpClient->post(
+                $this->getAluUrl($request->getMerchantConfig()->getPlatform()),
+                $requestParams
+            );
+            $xmlObject = new SimpleXMLElement($responseXML);
+
+        } catch (ConnectionException $e) {
+            echo($e->getMessage() . ' ' . $e->getCode());
+        } catch (\Exception $e) {
+            throw new ClientException($e->getMessage(), $e->getCode());
+        }
+        /*
+        $response = $this->getResponse($xmlObject);
+        if ('' != $response->getHash()) {
+            $hashService->validateResponseHash($response);
+        }
+        return $response;
+        */
+    }
+
+    /**
+     * @param string $platform
+     * @return string
+     * @throws ClientException
+     */
+    private function getAluUrl($platform)
+    {
+        if (!empty($this->customUrl)) {
+            return $this->customUrl;
+        }
+
+        if (!isset($this->aluUrlHostname[$platform])) {
+            throw new ClientException('Invalid platform');
+        }
+        return $this->aluUrlHostname[$platform] . self::ALU_URL_PATH;
     }
 }
