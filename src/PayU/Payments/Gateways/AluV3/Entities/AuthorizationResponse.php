27ef358e56a0df203682a2c25d73d4c759363b1e
spopawdodo <maria.denisa@gmail.com>
2020-03-16 12:55:54 +0200

    add AluV3/Entities/AuthorizationResponse

---
+++ src/PayU/Payments/Gateways/AluV3/Entities/AuthorizationResponse.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace AluV3\Entities;
+
+
+use SimpleXMLElement;
+
+class AuthorizationResponse
+{
+    /**
+     * @var SimpleXMLElement
+     */
+    private $response;
+
+    /**
+     * AuthorizationResponse constructor.
+     *
+     * @param SimpleXMLElement $response
+     */
+    public function __construct(SimpleXMLElement $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @return SimpleXMLElement
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+}
