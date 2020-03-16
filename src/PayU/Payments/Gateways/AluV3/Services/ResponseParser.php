eda6e51039a15a919bc2d6031b6f7f06bda6ec62
spopawdodo <maria.denisa@gmail.com>
2020-03-16 12:56:54 +0200

    add function ParseXmlResponse to ResponseParser.php

--- src/PayU/Payments/Gateways/AluV3/Services/ResponseParser.php
+++ src/PayU/Payments/Gateways/AluV3/Services/ResponseParser.php
@@ -4,7 +4,13 @@
 namespace AluV3\Services;
 
 
+use AluV3\Entities\AuthorizationResponse;
+use SimpleXMLElement;
+
 class ResponseParser
 {
-
+    public function parseXMLResponse($xmlResponse)
+    {
+        return new AuthorizationResponse(new SimpleXMLElement($xmlResponse));
+    }
 }
