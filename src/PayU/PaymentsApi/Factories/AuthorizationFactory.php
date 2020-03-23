940e84cf4148a115ffd37b17c84a865017058308
andra.simionov@payu.ro <andras.catalina@gmail.com>
2020-03-23 17:44:32 +0200

    Add payments V4 api version and example code for it

--- src/PayU/PaymentsApi/Factories/AuthorizationFactory.php
+++ src/PayU/PaymentsApi/Factories/AuthorizationFactory.php
@@ -2,6 +2,8 @@
 
 namespace PayU\PaymentsApi\Factories;
 
+use PaymentsV4\PaymentsV4;
+use PayU\Alu\Exceptions\ClientException;
 use PayU\Alu\HashService;
 use PayU\Alu\HTTPClient;
 use PayU\PaymentsApi\AluV3\AluV3;
@@ -10,11 +12,12 @@ use PayU\PaymentsApi\Exceptions\AuthorizationFactoryException;
 class AuthorizationFactory
 {
     /**
-     * @param string $apiVersion
+     * @param $apiVersion
      * @param HTTPClient $httpClient
      * @param HashService $hashService
-     * @return AluV3
+     * @return PaymentsV4|AluV3
      * @throws AuthorizationFactoryException
+     * @throws ClientException
      */
     public function create(
         $apiVersion,
@@ -24,6 +27,9 @@ class AuthorizationFactory
         switch ($apiVersion) {
             case AluV3::API_VERSION_V3:
                 return new AluV3($httpClient, $hashService);
+
+            case PaymentsV4::API_VERSION_V4:
+                return new PaymentsV4();
 
             default:
                 throw new AuthorizationFactoryException('Invalid API version provided.');
