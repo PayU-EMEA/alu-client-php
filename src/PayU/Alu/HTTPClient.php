a6489952b6c8c30dfcd4ef7c12e00bbefe354cfd
spopawdodo <maria.denisa@gmail.com>
2020-03-23 13:10:33 +0200

    update PhpDocs

--- src/PayU/Alu/HTTPClient.php
+++ src/PayU/Alu/HTTPClient.php
@@ -6,7 +6,8 @@ namespace PayU\Alu;
 /**
  * Class HTTPClient
  * @package PayU\Alu
- * @deprecated
+ * @deprecated Will be removed on a further library version.
+ * Depending on PaymentsApi version, should use AluV3/Services/HTTPClient or PaymentsV4/Services/HTTPClient
  */
 class HTTPClient extends \PayU\PaymentsApi\AluV3\Services\HTTPClient
 {
--- src/PayU/Alu/HashService.php
+++ src/PayU/Alu/HashService.php
@@ -6,7 +6,8 @@ namespace PayU\Alu;
 /**
  * Class HashService
  * @package PayU\Alu
- * @deprecated
+ * @deprecated Will be removed on a further library version.
+ * Depending on PaymentsApi version, should use AluV3/Services/HashService or PaymentsV4/Services/HashService
  */
 class HashService extends \PayU\PaymentsApi\AluV3\Services\HashService
 {
