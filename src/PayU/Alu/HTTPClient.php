cebddeb8eb30e45ede2b27597e070abac4cbae42
spopawdodo <maria.denisa@gmail.com>
2020-03-23 12:52:53 +0200

    add HashService.php and HTTPClient.php to PayU/Alu and mark them as deprecated

---
+++ src/PayU/Alu/HTTPClient.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace PayU\Alu;
+
+/**
+ * Class HTTPClient
+ * @package PayU\Alu
+ * @deprecated
+ */
+class HTTPClient extends \PayU\PaymentsApi\AluV3\Services\HTTPClient
+{
+
+}
---
+++ src/PayU/Alu/HashService.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace PayU\Alu;
+
+/**
+ * Class HashService
+ * @package PayU\Alu
+ * @deprecated
+ */
+class HashService extends \PayU\PaymentsApi\AluV3\Services\HashService
+{
+
+}
