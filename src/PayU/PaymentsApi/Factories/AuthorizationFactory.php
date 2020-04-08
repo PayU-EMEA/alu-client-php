4a4f537ad46f6e1ab18da1dd87486b0cb0e1cf97
andras.catalina@gmail.com <andras.catalina@gmail.com>
2020-04-08 10:32:20 +0300

    Add case for PaymentAPI v4

--- src/PayU/PaymentsApi/Factories/AuthorizationFactory.php
+++ src/PayU/PaymentsApi/Factories/AuthorizationFactory.php
@@ -2,20 +2,22 @@
 
 namespace PayU\PaymentsApi\Factories;
 
+use PaymentsV4\PaymentsV4;
 use PayU\Alu\HashService;
 use PayU\Alu\HTTPClient;
 use PayU\PaymentsApi\AluV3\AluV3;
 use PayU\PaymentsApi\Exceptions\AuthorizationFactoryException;
-use PayU\PaymentsApi\Interfaces\AuthorizationInterface;
 
 final class AuthorizationFactory
 {
+
     /**
-     * @param string $apiVersion
+     * @param $apiVersion
      * @param HTTPClient $httpClient
      * @param HashService $hashService
-     * @return AuthorizationInterface
+     * @return PaymentsV4|AluV3
      * @throws AuthorizationFactoryException
+     * @throws \PayU\Alu\Exceptions\ClientException
      */
     public function create(
         $apiVersion,
@@ -25,6 +27,9 @@ final class AuthorizationFactory
         switch ($apiVersion) {
             case AluV3::API_VERSION_V3:
                 return new AluV3($httpClient, $hashService);
+
+            case PaymentsV4::API_VERSION_V4:
+                return new PaymentsV4();
 
             default:
                 throw new AuthorizationFactoryException('Invalid API version provided.');
