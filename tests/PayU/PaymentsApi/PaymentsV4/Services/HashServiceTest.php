519908ef8b0afb22a64b217abecf49fd379c7d36
spopawdodo <maria.denisa@gmail.com>
2020-04-24 15:07:24 +0300

    split jsonString

--- tests/PayU/PaymentsApi/PaymentsV4/Services/HashServiceTest.php
+++ tests/PayU/PaymentsApi/PaymentsV4/Services/HashServiceTest.php
@@ -31,7 +31,50 @@ class HashServiceTest extends \PHPUnit_Framework_TestCase
         return array(
             array(
                 '2020-04-10T15:15:08+00:00',
-                '{"merchantPaymentReference":"1112","currency":"RON","returnUrl":"http:\/\/path\/to\/your\/returnUrlScript","authorization":{"paymentMethod":"CCVISAMC","installmentsNumber":null,"cardDetails":{"number":"4111111111111111","expiryMonth":"01","expiryYear":"2026","cvv":"123","owner":"Card Owner Name"}},"client":{"billing":{"firstName":"FirstName","lastName":"LastName","email":"john.doe@mail.com","phone":"40123456789","city":"City","countryCode":"RO","state":null,"companyName":null,"taxId":null,"addressLine1":"Address1","addressLine2":"Address2","zipCode":null,"identityDocument":{"number":"111222","type":null}},"delivery":{"firstName":"FirstName","lastName":"LastName","phone":"40123456789","addressLine1":"Address1","addressLine2":"Address2","zipCode":null,"city":"City","state":null,"countryCode":"RO","email":"john.doe@mail.com"},"ip":"127.0.0.1","time":""},"products":[{"name":"PNAME01","sku":"PCODE01","unitPrice":100,"quantity":1,"additionalDetails":null,"vat":24},{"name":"PNAME02","sku":"PCODE02","unitPrice":200,"quantity":1,"additionalDetails":null,"vat":24}]}',
+                '{"merchantPaymentReference":"1112",'.
+                '"currency":"RON",'.
+                '"returnUrl":"http:\/\/path\/to\/your\/returnUrlScript",'.
+                '"authorization":{'.
+                '"paymentMethod":"CCVISAMC",'.
+                '"installmentsNumber":null,'.
+                '"cardDetails":{'.
+                '"number":"4111111111111111",'.
+                '"expiryMonth":"01",'.
+                '"expiryYear":"2026",'.
+                '"cvv":"123",'.
+                '"owner":"Card Owner Name"}},'.
+                '"client":{'.
+                '"billing":'.
+                '{"firstName":"FirstName",'.
+                '"lastName":"LastName",'.
+                '"email":"john.doe@mail.com",'.
+                '"phone":"40123456789",'.
+                '"city":"City",'.
+                '"countryCode":"RO",'.
+                '"state":null,'.
+                '"companyName":null,'.
+                '"taxId":null,'.
+                '"addressLine1":"Address1",'.
+                '"addressLine2":"Address2",'.
+                '"zipCode":null,'.
+                '"identityDocument":{'.
+                '"number":"111222",'.
+                '"type":null}},'.
+                '"delivery":{'.
+                '"firstName":"FirstName",'.
+                '"lastName":"LastName",'.
+                '"phone":"40123456789",'.
+                '"addressLine1":"Address1",'.
+                '"addressLine2":"Address2",'.
+                '"zipCode":null,'.
+                '"city":"City",'.
+                '"state":null,'.
+                '"countryCode":"RO",'.
+                '"email":"john.doe@mail.com"},'.
+                '"ip":"127.0.0.1","time":""},'.
+                '"products":[{'.
+                '"name":"PNAME01","sku":"PCODE01","unitPrice":100,"quantity":1,"additionalDetails":null,"vat":24},'.
+                '{"name":"PNAME02","sku":"PCODE02","unitPrice":200,"quantity":1,"additionalDetails":null,"vat":24}]}',
                 '4ad6a9925c94bb41afa79f2ad44b5b4ae307ab356b5e005f371a8c3bb53cc3fc'
             ),
         );
