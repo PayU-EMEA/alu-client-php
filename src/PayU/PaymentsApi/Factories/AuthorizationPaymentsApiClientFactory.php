2fc65ce254c277d18ac1ebaf106c3e80890dba39
spopawdodo <maria.denisa@gmail.com>
2020-04-30 11:39:08 +0300

    add PaymentsV4.php to factory class

--- src/PayU/PaymentsApi/Factories/AuthorizationPaymentsApiClientFactory.php
+++ src/PayU/PaymentsApi/Factories/AuthorizationPaymentsApiClientFactory.php
@@ -6,7 +6,9 @@ use PayU\Alu\Exceptions\ClientException;
 use PayU\PaymentsApi\AluV3\AluV3;
 use PayU\PaymentsApi\AluV3\Services\HashService;
 use PayU\PaymentsApi\AluV3\Services\HTTPClient;
+use PayU\PaymentsApi\Exceptions\AuthorizationException;
 use PayU\PaymentsApi\Exceptions\AuthorizationPaymentsApiClientFactoryException;
+use PayU\PaymentsApi\PaymentsV4\PaymentsV4;
 
 /**
  * Responsible with creating a different implementation for AuthorizationPaymentsApiClient interface, depending on the
@@ -21,9 +23,10 @@ final class AuthorizationPaymentsApiClientFactory
      * @param string $secretKey
      * @param HTTPClient|null $httpClient
      * @param HashService|null $hashService
-     * @return AluV3
+     * @return AluV3 | PaymentsV4
      * @throws AuthorizationPaymentsApiClientFactoryException
      * @throws ClientException
+     * @throws AuthorizationException
      */
     public function createPaymentsApiClient(
         $apiVersion,
@@ -41,7 +44,8 @@ final class AuthorizationPaymentsApiClientFactory
                     $hashService = new HashService($secretKey);
                 }
                 return new AluV3($httpClient, $hashService);
-
+            case PaymentsV4::API_VERSION_V4:
+                return new PaymentsV4();
             default:
                 throw new AuthorizationPaymentsApiClientFactoryException('Invalid API version provided.');
         }
