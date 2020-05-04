fe796c5b8ccac435c554410eda439d8fda4e1f00
spopawdodo <maria.denisa@gmail.com>
2020-05-04 11:06:48 +0300

    add post method for tokenV2

--- src/PayU/PaymentsApi/PaymentsV4/Services/HTTPClient.php
+++ src/PayU/PaymentsApi/PaymentsV4/Services/HTTPClient.php
@@ -100,4 +100,35 @@ class HTTPClient
             'Content-Type: application/json;charset=utf-8'
         ];
     }
+
+    /**
+     * @param $url
+     * @param array $postParams
+     * @return string
+     * @throws ConnectionException
+     */
+    public function postTokenCreationRequest($url, array $postParams)
+    {
+        curl_setopt_array(
+            $this->handler,
+            array(
+                CURLOPT_URL => $url,
+                CURLOPT_POST => true,
+                CURLOPT_POSTFIELDS => http_build_query($postParams),
+                CURLOPT_HTTPHEADER => array('Content-Type: application/x-www-form-urlencoded')
+            )
+        );
+
+        $result = curl_exec($this->handler);
+        if (curl_errno($this->handler) > 0) {
+            throw new ConnectionException(
+                sprintf(
+                    'Curl error "%s" when accessing url: "%s"',
+                    curl_error($this->handler),
+                    $url
+                )
+            );
+        }
+        return $result;
+    }
 }
